5a05090421f80dfe22ed3e862d1c3df9be4f5320
Vijayasri <tanmaivijayasri201106@gmail.com>
2026-03-15 15:39:06 +0530

    modified

--- AI_powered_interview_project/Server.js
+++ AI_powered_interview_project/Server.js
@@ -787,7 +787,7 @@ Return STRICT JSON only:
 
         const nextQPrompt = `
             const nextQPrompt = `
-            Generate the next interview question.
+            //Generate the next interview question.
 
             Mode: ${context.mode}
 
